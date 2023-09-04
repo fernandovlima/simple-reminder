ad70526cbb6e27122ac72cd37e723b007bd81672
fernandovlima <fernandovlima.s@gmail.com>
2023-09-04 12:59:16 -0300

    update README.md

--- middleware.ts
+++ middleware.ts
@@ -5,18 +5,7 @@ import { NextResponse } from "next/server";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  afterAuth(auth, req) {
-    // handle users who aren't authenticated
-    if (!auth.userId && !auth.isPublicRoute) {
-      return redirectToSignIn({ returnBackUrl: req.url });
-    }
-
-    // handle users who are authenticated
-    if (auth.userId && req.nextUrl.basePath === "/") {
-      const dashboardUrl = new URL("/dashboard", req.url);
-      return NextResponse.redirect(dashboardUrl);
-    }
-  },
+  publicRoutes: ["/"],
 });
 
 export const config = {
