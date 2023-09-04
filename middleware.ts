9484f510225b810e1c2998b2dc57be5e33c83aaf
fernandovlima <fernandovlima.s@gmail.com>
2023-09-04 13:58:00 -0300

    handle auth middleware redirects

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,19 @@ import { NextResponse } from "next/server";
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-export default authMiddleware({});
+export default authMiddleware({
+  afterAuth(auth, req, evt) {
+    // handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+    // redirect them to organization selection page
+    if (auth.userId && req.nextUrl.pathname !== "/dashboard") {
+      const dashboard = new URL("/dashboard", req.url);
+      return NextResponse.redirect(dashboard);
+    }
+  },
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
